d92ff5c0e502b411bc8020b6d4cd1c6e29a16f3d
SYD0831 <yu.22@intl.zju.edu.cn>
2023-03-15 14:43:42 +0800

    “Initialise”

--- Practical5/breed_like_rabbits.py
+++ Practical5/breed_like_rabbits.py
@@ -7,11 +7,14 @@ while number_rabbit <= 100:
 #	Check if there are enough rabbits
 #	if no: keep breeding
 #	calculate the number of newborn rabbits
-	number_new = number_rabbit*2
+	number_new = number_rabbit
 #	calculate the total number of rabbits
 	number_rabbit = number_rabbit + number_new
 #	Calculate the number of generations
 	generation += 1
 #If yes: output result
 generation_out = str(generation)
-print("the",generation_out,"st has more than 100 rabbits") 
+print("the",generation_out,"st genoration has more than 100 rabbits") 
+
+#the result of these codes :the 7 st genoration has more than 100 rabbits
+
