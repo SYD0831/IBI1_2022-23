133eac013033c01551afd7b1eab585ec37d9f7e4
SYD0831 <yu.22@intl.zju.edu.cn>
2023-03-22 16:23:38 +0800

    “Initialise”

--- Practical6/Favourite_movie_genres.py
+++ Practical6/Favourite_movie_genres.py
@@ -1,12 +1,16 @@
 #creat a dictionary to record data.
 #use the for loop to print the dictionar.
-
 #use three variables to store data 
 #use the plt.pie to draw the figure
-
-#use input() to gain the opinion of the user
+#gain the specific movie
 #Link to the corresponding value through the dictionary
 #print the output
+
+#you can select the type of movie you interest in the following
+#'Comedy', 'Action', 'Romance', 'Fantasy', 'Science-fiction', 'Horror', 'Crime', 'Documentary', 'History', 'War'
+#then you should change the value of the a here
+a = 'Comedy'
+
 
 Favourite_movie_genres = {'Comedy':73, 'Action':42, 'Romance':38,
                           'Fantasy':28, 'Science-fiction':22, 'Horror':19, 'Crime':18, 'Documentary':12,
@@ -16,9 +20,6 @@ for key in Favourite_movie_genres.keys():
     value =  Favourite_movie_genres[key]
     print("key:", key, "; value:", value)
 
-
-
-
 import matplotlib.pyplot as plt         
 types = 'Comedy', 'Action', 'Romance', 'Fantasy', 'Science-fiction', 'Horror', 'Crime', 'Documentary', 'History', 'War'
 sizes = [73, 42, 38, 28, 22, 19, 18, 12, 8, 7]
@@ -27,9 +28,10 @@ plt.pie(sizes, explode=explode, labels=types, autopct='%1.1f%%', shadow=False, s
 plt.axis('equal')
 plt.show()
 
- #Interactive variable
-a = input('please give me a type of movie: ' )         
 print('the number of students who prefer', a, 'is', Favourite_movie_genres[a])
+
+#Interactive variable
+#a = input('please give me a type of movie: ' )         
 
 
 
--- Practical6/Olympic Costs.py
+++ Practical6/Olympic Costs.py
@@ -1,9 +1,17 @@
-#use input() to gain the correlated value of the cost of hosting the Summer Olympic Games in a list
-#Convert the user input into a list of integers#sort the list
-       #like 'costs=[1,8,15,7,5,14,43,40]'
-       #the type of the input() is string , we will choose the number separated by commas
-       #convert the string into a list 
+#You need to enter your cost here
+#you can change the variable number_list
+cost = [1,8,15,7,5,14,43,40]
+number_list = cost
+
+#You need to enter your place and year here
+#you can change the variable place
+place = ['Los Angeles 1984', 'Seoul 1988', 'Barcelona 1992', 'Atlanta 1996', 'Sydney 2000', 'Athens 2003', 'Beijing 2008', 'London 2012']
+
+
+#creat a dictionary to ralated the cost and place
+#Give variable lable correct value
 #Sort the numbers in the list
+#correct the place in correct turn
 #print the sorted list 
 
 #draw the figure
@@ -15,19 +23,34 @@
 # Add the x, y axin lables 
 # Display the plot
 
-user_input = input("Enter a list of numbers separated by commas without blank like 'costs=[1,8,15,7,5,14,43,40]' : ")
-user_input = user_input[7:-1]
-number_list = [int(num) for num in user_input.split(',')]
+my_dict = dict(zip(number_list, place))
+labels = []
 number_list.sort()
+for i in number_list:
+    labels.append(my_dict[i])
+
 print("Sorted list: ", number_list)
 
 
 import matplotlib.pyplot as plt         
 colors = ['violet', 'indigo', 'blue', 'lime', 'green', 'yellow', 'orange', 'red']
-labels = ['1', '2', '3', '4', '5', '6', '7', '8']
 plt.barh(range(len(number_list)), number_list, color=colors)
 plt.yticks(range(len(number_list)), labels)
 plt.title('Cost of Hosting Summer Olympic Games')
 plt.xlabel('Cost in Billions of dollar')
 plt.ylabel('Cost')
 plt.show()
+
+
+
+
+
+#use input() to gain the correlated value of the cost of hosting the Summer Olympic Games in a list
+#Convert the user input into a list of integers#sort the list
+       #like 'costs=[1,8,15,7,5,14,43,40]'
+       #the type of the input() is string , we will choose the number separated by commas
+       #convert the string into a list 
+
+# user_input = input("Enter a list of numbers separated by commas without blank like 'costs=[1,8,15,7,5,14,43,40]' : ")
+#user_input = user_input[7:-1]
+#number_list = [int(num) for num in user_input.split(',')]
